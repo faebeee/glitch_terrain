1380c3f97399dfe1faa69774bb5cbd8fce724da9
fabs <faebeee@gmail.com>
2020-08-16 18:45:43 +0200

    Improve randomness

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 import * as THREE from 'https://unpkg.com/three@0.119.1/build/three.module.js';
 
-const COLORS = { BG: 0xffffff, MATERIAL:  0xffffff};
-var width = window.innerWidth;
-var height = window.innerHeight;
+const COLORS = { BG: 0xffffff, MATERIAL: 0xffffff };
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 // Setup the scene
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(45, width / height, 1, 3000);
-var cameraTarget = { x: 0, y: 0, z: 0 };
-camera.position.y = 170;
-camera.position.z = 1000;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, width / height, 1, 3000);
+const cameraTarget = { x: 0, y: 0, z: 0 };
+camera.position.y = Math.random() * 300;
+camera.position.z = Math.random() * 1000;
 camera.rotation.x = -15 * Math.PI / 180;
 camera.lookAt(cameraTarget.x, cameraTarget.y, cameraTarget.z);
 
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
-var light = new THREE.DirectionalLight(COLORS.BG, 1.3);
+const light = new THREE.DirectionalLight(COLORS.BG, 1.3);
 light.position.set(camera.position.x, camera.position.y + 500, camera.position.z + 500).normalize();
 scene.add(light);
 
 // Setup the terrain
-var geometry = new THREE.PlaneBufferGeometry(2000, 2000, 256, 256);
-var material = new THREE.MeshLambertMaterial({ color: COLORS.MATERIAL, wireframe: true });
-var terrain = new THREE.Mesh(geometry, material);
+const geometry = new THREE.PlaneBufferGeometry(2000, 2000, 256, 256);
+const material = new THREE.MeshLambertMaterial({ color: COLORS.MATERIAL, wireframe: true });
+const terrain = new THREE.Mesh(geometry, material);
 terrain.rotation.x = -Math.PI / 2;
 scene.add(terrain);
 
-var perlin = new Perlin();
-var peak = 60;
-var smoothing = 300;
+const perlin = new Perlin();
+const peak = Math.random() * 100 + 50;
+const smoothing = Math.random() * 500 + 200;
 
 function refreshVertices() {
     var vertices = terrain.geometry.attributes.position.array;
@@ -45,20 +45,5 @@ function refreshVertices() {
     terrain.geometry.computeVertexNormals();
 }
 
-var clock = new THREE.Clock();
-var movementSpeed = 60;
-
-function update() {
-    var delta = clock.getDelta();
-    terrain.position.z += movementSpeed * delta;
-    camera.position.z += movementSpeed * delta;
-    refreshVertices();
-}
-
-function render() {
-    renderer.render(scene, camera);
-}
-
-
-update();
-render();
+refreshVertices();
+renderer.render(scene, camera);
